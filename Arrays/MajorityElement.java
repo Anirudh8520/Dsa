bc4c98a6201eff7d299e028e08fc920702697f1f
Anirudh8520 <omanirudh2006@gmail.com>
2026-06-13 19:40:21 +0530

    Day 1

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MajorityElement {
+    public static void main(String[] args) {
+         int arr[] = {3,2,3};
+        int n = arr.length;
+        int count = 1;
+        int majority = arr[0];
+        for(int i=0; i<n; i++) {
+            if(arr[i] == majority) {
+                count ++;
+            } else {
+                count --;
+            }
+            if(count == 0) {
+                majority = arr[i];
+                count = 1;
+            }
+        }
+        System.out.println(majority);
+          
+    }
+}
