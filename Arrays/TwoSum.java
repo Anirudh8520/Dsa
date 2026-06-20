4c1dd569568b0bae72f5e3fce519880ca9bb9f15
Anirudh8520 <omanirudh2006@gmail.com>
2026-06-20 21:46:39 +0530

    Day 2

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class TwoSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        HashMap<Integer , Integer> map = new HashMap<>();
         System.out.println("enter the size");
         int n = sc.nextInt();
         System.out.println("enter target:");
@@ -9,13 +10,17 @@ public class TwoSum {
         int arr[] = new int[n];
         for(int i=0; i<n; i++) {
             arr[i] = sc.nextInt(); 
-        }
-        for(int i=0; i<n; i++) {
-            for(int j = i+1; j<n; j++) {
-                if(arr[i]+arr[j] == target) {
-                    System.out.println("The two indices are:" + i + " "+ j);
-                }
-            }
-        }
+        
     }
+    for(int i=0; i<n; i++) {
+        //pairsum = first + second = target
+        int second = target - arr[i];
+        if(map.containsKey(second)) {
+            System.out.println("The indices are: " + map.get(second) + " and " + i);
+            break;
+        }
+        map.put(arr[i],i);
+    }
+
+}
 }
